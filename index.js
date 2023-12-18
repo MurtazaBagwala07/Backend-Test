9d6bd8aa95514363c2106ddc9f99f4e725110e1d
MurtazaBagwala07 <murtazabag786@gmail.com>
2023-12-18 21:31:09 +0530

    continued learning backend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("listening on port" + port);
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
 });
 
 const exampleJson = { name: "John", age: 30, car: null };
